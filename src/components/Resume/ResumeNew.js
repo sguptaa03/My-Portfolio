431624aced55e14922a1da3025afe192b3864a9d
Sakshi Gupta <you@example.com>
2023-08-17 17:17:11 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "/Users/sakshi/Desktop/my-portfolio/src/Assets/sakshi_gupta_resume.pdf";
+import pdf from "/Users/sakshi/Desktop/my-portfolio/src/Assets/sakshi-gupta-frontend-developed-resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
